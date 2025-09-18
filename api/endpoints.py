878e3207a81848a4963c556fe19a3d7760589d24
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-09-18 10:05:10 +0300

    Update endpoints.py

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,1 +1,9 @@
-Жульен14
+class Endpoints:
+    create_courier = "/api/v1/courier"
+    login_courier = "/api/v1/courier/login"
+    create_order = "/api/v1/orders"
+    delete_courier = "/api/v1/courier/"
+    get_orders_list = "/api/v1/orders"
+    accept_order_by_number = "/api/v1/orders/track"
+    accept_order = "/api/v1/orders/accept/"
+    cancel_order = "/api/v1/orders/cancel"
