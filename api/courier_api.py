c0da77eb8a09f6d595289d283df8b4eb9679dd7d
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-09-18 10:04:36 +0300

    Update courier_api.py

--- api/courier_api.py
+++ api/courier_api.py
@@ -1,2 +1,12 @@
-Жульен15
-# Courier API methods and functionality
+from api.base_api import BaseAPI
+from api.endpoints import Endpoints
+
+class CourierAPI(BaseAPI):
+    def create_courier(self, data):
+        return self.post(Endpoints.create_courier, json=data)
+    
+    def login_courier(self, data):
+        return self.post(Endpoints.login_courier, json=data)
+    
+    def delete_courier(self, courier_id):
+        return self.delete(f"{Endpoints.delete_courier}{courier_id}")
