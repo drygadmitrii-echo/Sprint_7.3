bf8a5e4bb1085727ce86f2fcba695685b28dba37
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-09-18 10:08:29 +0300

    Update data.py

--- data/data.py
+++ data/data.py
@@ -1,2 +1,26 @@
-Жульен12
-# Test data and constants
+COURIER_DATA = {
+    "login": "test_courier_login",
+    "password": "test_courier_password",
+    "firstName": "test_courier_firstname"
+}
+
+ORDER_DATA = {
+    "firstName": "Александр",
+    "lastName": "Барон",
+    "address": "Авиамоторная улица, 8А",
+    "metroStation": 8,
+    "phone": "+79206709875",
+    "rentTime": 2,
+    "deliveryDate": "2025-09-16",
+    "comment": "И поживее!",
+    "color": ["BLACK"]
+}
+
+ERROR_MESSAGES = {
+    "login_already_used": "Этот логин уже используется",
+    "not_enough_data": "Недостаточно данных для создания учетной записи",
+    "not_enough_login_data": "Недостаточно данных для входа",
+    "account_not_found": "Учетная запись не найдена",
+    "not_enough_search_data": "Недостаточно данных для поиска",
+    "order_not_found": "Заказ не найден"
+}
