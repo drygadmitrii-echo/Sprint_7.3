f9218dd32efbd5e526a2bac0d1a4266bf38b373a
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-09-18 09:13:39 +0300

    Update test_order_list.py

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -1,13 +1,1 @@
-import pytest
-import requests
-
-
-class TestOrderList:
-    BASE_URL = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
-
-    def test_get_order_list(self):
-        response = requests.get(self.BASE_URL)
-
-        assert response.status_code == 200
-        assert "orders" in response.json()
-        assert isinstance(response.json()["orders"], list)
+Жульен4
