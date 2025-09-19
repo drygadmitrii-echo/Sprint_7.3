af10a36d73ec3dd0bf6c4ef4657f7a1c17864fc2
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-09-19 12:02:15 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,1 +1,19 @@
-"create new file"
+import pytest
+import allure
+from helpers.helper import register_new_courier, delete_courier
+from api.courier_api import CourierAPI
+
+@pytest.fixture
+@allure.step("Создание и удаление тестового курьера")
+def create_and_delete_courier():
+    courier_api = CourierAPI()
+    courier_data = register_new_courier()
+    yield courier_data
+
+    login_response = courier_api.login_courier({
+        "login": courier_data["login"],
+        "password": courier_data["password"]
+    })
+    if login_response.status_code == 200 and "id" in login_response.json():
+        courier_id = login_response.json()["id"]
+        delete_courier(courier_id)
