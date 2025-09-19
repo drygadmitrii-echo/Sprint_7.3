59ab13c09dc8c7936096edbd66002a98fc5eae85
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-09-19 11:50:55 +0300

    Update test_order_list.py

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -1,14 +1,1 @@
-import pytest
-import allure
-from api.order_api import OrderAPI
-
-@allure.feature("Список заказов")
-class TestOrderList:
-    order_api = OrderAPI()
-
-    @allure.title("Получение списка заказов")
-    def test_get_order_list(self):
-        response = self.order_api.get_orders_list()
-        assert response.status_code == 200
-        assert "orders" in response.json()
-        assert isinstance(response.json()["orders"], list)
+"create new file"
